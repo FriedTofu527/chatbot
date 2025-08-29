1ed1249c3ae4313b65745c11e183083b6b77c099
FriedTofu527 <geoffreyhuang2005@gmail.com>
2025-08-29 12:34:46 -0700

    optimized parser

--- get_documents.py
+++ get_documents.py
@@ -4,6 +4,7 @@ import requests
 import bs4
 
 
+PARSER = 'lxml'
 TRIM_THRESHOLD = 20
 DEBUG = True
 FAQ_SOURCES = ['https://www.caccusa.org/faq.php?p=1', 
@@ -23,88 +24,42 @@ PROGRAMS_SOURCES = ['https://www.caccusa.org/program.php?p=10',
                    'https://www.caccusa.org/program.php?p=16']
 
 
-def html_parser(root: bs4.Tag, tag: str, recursive: bool, trim: bool) -> list[str]:
-    documents = []
-    
-    for target in root.find_all(tag, recursive=recursive):
-        document = ' '.join(target.stripped_strings)
-        if not trim or len(document) > TRIM_THRESHOLD:
-            documents.append(document)
-
-    return documents
-
-
-def page_getter(urls: list[str]) -> list[str]:
-    pages = []
+def page_getter(urls: list[str]) -> dict[str, str]:
+    pages = {}
 
     for url in urls:
         response = requests.get(url=url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15'})
         if response.status_code == 200:
-            pages.append(response.text)
+            pages[url] = response.text
         else:
             if DEBUG:
-                print(f'Connection Error. Skipping URL={url}')
-            pages.append('')
+                print(f'Connection error. Skipping URL = {url}')
     return pages
 
 
-def faq_parser() -> list[str]:
+def page_parser(sources: list[str], search: tuple[str, dict], tag: str, recursive: bool, trim: bool) -> list[str]:
     documents = []
+    pages = page_getter(sources)
 
-    for faq in page_getter(FAQ_SOURCES):
-        for table in bs4.BeautifulSoup(faq, 'lxml').find_all('table'):
-            if isinstance(table, bs4.Tag):
-                documents.extend(html_parser(table, 'tr', True, True))
+    for url in sources:
+        for result in bs4.BeautifulSoup(pages[url], PARSER).find_all(*search):
+            if isinstance(result, bs4.Tag):
+                for target in result.find_all(tag, recursive=recursive):
+                    document = ' '.join(target.stripped_strings)
+                    if not trim or len(document) > TRIM_THRESHOLD:
+                        documents.append(document)
             else:
                 if DEBUG:
-                    print('Parsing error. Skipping FAQ page.')
-    return documents
-
-
-def about_parser() -> list[str]:
-    documents = []
-
-    for about in page_getter(ABOUT_SOURCES):
-        for main in bs4.BeautifulSoup(about, 'lxml').find_all('main'):
-            if isinstance(main, bs4.Tag):
-                documents.extend(html_parser(main, 'article', True, True))
-            else:
-                if DEBUG:
-                    print('Parsing error. Skipping about page.')
-    return documents
-
-
-def seminar_parser() -> list[str]:
-    documents = []
-
-    for seminar in page_getter(SEMINAR_SOURCES):
-        for body in bs4.BeautifulSoup(seminar, 'lxml').find_all('body'):
-            if isinstance(body, bs4.Tag):
-                documents.extend(html_parser(body, 'div', False, True))
-            else:
-                if DEBUG:
-                    print('Parsing error. Skipping seminar page.')
-    return documents
-
-
-def general_information_parser() -> list[str]:
-    documents = []
-
-    for gi in page_getter(GENERAL_INFORMATION_SOURCES):
-        for div in bs4.BeautifulSoup(gi, 'lxml').find_all('div', {'class': 'panel panel-body'}):
-            if isinstance(div, bs4.Tag):
-                documents.extend(html_parser(div, 'div', False, True))
-            else:
-                if DEBUG:
-                    print('Parsing error. Skipping general information page.')
+                    print(f'Parsing error. Skipping URL = {url}')
     return documents
 
 
 def programs_parser() -> list[str]:
     documents = []
+    pages = page_getter(PROGRAMS_SOURCES)
 
-    for program in page_getter(PROGRAMS_SOURCES):
-        table = bs4.BeautifulSoup(program, 'lxml').find('table', {'class': 'table table-condensed table-borderless'})
+    for url in PROGRAMS_SOURCES:
+        table = bs4.BeautifulSoup(pages[url], PARSER).find('table', {'class': 'table table-condensed table-borderless'})
         if isinstance(table, bs4.Tag):
             rows = table.find_all('tr', recursive=False)
             for i in range(0, len(rows), 3):
@@ -125,17 +80,17 @@ def programs_parser() -> list[str]:
                 documents.append(' '.join(en))
         else:
             if DEBUG:
-                print('Parsing error. Skipping program page.')
+                print(f'Parsing error. Skipping URL = {url}.')
     return documents
 
 
 def run() -> None:
     documents = []
 
-    documents.extend(faq_parser())
-    documents.extend(about_parser())
-    documents.extend(seminar_parser())
-    documents.extend(general_information_parser())
+    documents.extend(page_parser(FAQ_SOURCES, ('table', {}), 'tr', True, True))
+    documents.extend(page_parser(ABOUT_SOURCES, ('main', {}), 'article', True, True))
+    documents.extend(page_parser(FAQ_SOURCES, ('body', {}), 'div', False, True))
+    documents.extend(page_parser(FAQ_SOURCES, ('div', {'class': 'panel panel-body'}), 'div', False, True))
     documents.extend(programs_parser())
 
     print(len(documents))
