67763415f41b9ff3901abf572355bf776e9821fd
FriedTofu527 <geoffreyhuang2005@gmail.com>
2025-09-08 21:05:09 -0700

    fixed crashing of query was empty

--- web_server.py
+++ web_server.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI
 from openai import OpenAI
 import chromadb
+import uvicorn
+
 import run
 
 
+API = FastAPI()
 CLIENT = OpenAI()
 COLLECTION = chromadb.PersistentClient().get_collection('collection')
 GENERATING_PROMPT = run.load_prompt('prompts/generating.txt')
 REWRITING_PROMPT = run.load_prompt('prompts/rewriting.txt')
-API = FastAPI()
 
 
 @API.get('/query')
 async def query(q: str) -> dict[str, str]:
-    rewritten_queries = run.rewrite_queries(CLIENT, REWRITING_PROMPT, q)
-    embedded_rewritten_queries = run.embed_queries(CLIENT, rewritten_queries)
-    documents = run.query_database(COLLECTION, embedded_rewritten_queries)
-    return {'query': run.generate_answer(CLIENT, GENERATING_PROMPT, documents, q)}
+    if q:
+        rewritten_queries = run.rewrite_queries(CLIENT, REWRITING_PROMPT, q)
+        embedded_rewritten_queries = run.embed_queries(CLIENT, rewritten_queries)
+        documents = run.query_database(COLLECTION, embedded_rewritten_queries)
+        return {'text': run.generate_answer(CLIENT, GENERATING_PROMPT, documents, q), 'error': ''}
+    else:
+        return {'text': '', 'error': 'Query string cannot be empty.'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(API, host='0.0.0.0', port=25565)
